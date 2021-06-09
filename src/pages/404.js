4221f116c53f394b3a3c54dd8edd0d805a4bcafc
Brian <byao321@gmail.com>
2021-06-08 22:00:40 -0400

    Add error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,15 @@
-// import * as React from 'react';
+import * as React from 'react';
 
-// import Seo from '../components/seo';
+import Seo from '../components/seo';
 
-// const NotFoundPage = () => {
-//     return (
-//         <div>
-//             <Seo title="404: Not found" />
-//             <h1>404: Not Found</h1>
-//             <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-//         </div>
-//     );
-// };
+const NotFoundPage = () => {
+    return (
+        <div>
+            <Seo title="404: Not found" />
+            <h1>404: Not Found</h1>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </div>
+    );
+};
 
-// export default NotFoundPage;
+export default NotFoundPage;
