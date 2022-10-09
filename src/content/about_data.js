238e115d2e70fc517883755266bde107bed9d228
Brian Yao <byao321@gmail.com>
2022-10-09 12:07:39 -0400

    Update project details

--- src/content/about_data.js
+++ src/content/about_data.js
@@ -2,7 +2,7 @@ function aboutData() {
     return [
         {
             section: 'About',
-            text: `<p>Over 4 years of professional experience in front-end development using HTML/HTML5, CSS/CSS3, JavaScript, TypeScript, React, React Router, Redux, jQuery, Express, Node.js, JSON, XML, and various other web development tools to build cutting-edge web applications.</p>
+            text: `<p>OExperience in front-end development using HTML/HTML5, CSS/CSS3, JavaScript, TypeScript, React, React Router, Redux, jQuery, Express, Node.js, JSON, XML, and various other web development tools to build cutting-edge web applications.</p>
             <br>
             <p>I am passionate about learning new technologies and incorporating them in my personal projects. Aside from web development, I am also deeply interested in data science and user design. I believe that a minimalistic design paired with reliable functionality is the key to delivering a good product. Please contact me at <a href = "mailto: byao.dev@gmail.com">byao.dev@gmail.com</a>, if you are interested in working with me.</p>
             <br>
@@ -17,9 +17,7 @@ function aboutData() {
                 <h4>Accomplishments and Working Experience:</h4>
                 <ul>
                 <li>Graduated Rutgers University with B.S. Computer Science</li>
-                <li>Cedar Pay: Cedar Pay brings more users to the checkout page by providing a billing experience built for consumers. Cedar Pay's patient-first digital experience is designed with empathy and accessibility in mind, leading to improved bill clarity, higher engagement and more payments. Cedar Pay guides patients to the best possible billing outcomes.  With features like denial notifications, insurance capture, live chat, personalized discounts and payment plans, give patients self-service solutions beyond payment to alleviate inbound call volume and improve long-term collections. Cedar Pay offers patients modern ways to pay their bills.</li>
-                <li>K Health: K Health is a digital healthcare company on a mission to use the power of shared medical knowledge to get everyone access to better, more affordable healthcare. The Primary Care Management Platform is a telemedicine platform that consolidates the knowledge of thousands of doctors and billions of clinical data insights to get people trusted information and treatment. The platform features smarter, faster, virtual intake process that gets doctors the info they need to care for the whole patient.</li>
-                <li>Policygenius: The Online Insurance Quoting Application is a convenient online application that removes virtually all of the hassle involved in determining an individual’s insurance quote. The application allows the automation of much of the quoting process online. Potential clients can use it to compare rates and quotes from many different insurance companies to choose the best one for themselves by saving time and energy. With the application, customers no longer need to divulge endless streams of personal data.</li>
+                <li>PayPal: Software engineer for i18n globalization team. Experience with Javascript, Node, Git, Agile.</li>
                 <li>Comcast: Completed 6-month internship in Data Science, Data Engineering, and Machine Learning.</li>
                 <li>Open source contributor.</li>
                 </ul>
